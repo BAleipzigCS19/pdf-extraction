77bedb4da1d9a9586acb1048304945ae2698d4d1
krankkk <59603675+krankkkk@users.noreply.github.com>
2021-10-06 02:54:25 +0200

    test-Workflows

--- Client/build.gradle.kts
+++ Client/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    java
     application
     id("org.openjfx.javafxplugin") version "0.0.10"
 }
@@ -20,7 +19,7 @@ javafx {
 }
 
 dependencies {
-    implementation(project(":Backend"))
+    //implementation(project(":Backend"))
 
     // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
