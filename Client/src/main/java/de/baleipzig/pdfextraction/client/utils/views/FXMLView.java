2c55a018d74dc46ec11b15c2a5d0fa8a5b19027c
krankkk <59603675+krankkkk@users.noreply.github.com>
2021-10-07 01:26:04 +0200

    PAN-19 Progress so far

--- Client/src/main/java/de/baleipzig/pdfextraction/client/utils/views/FXMLView.java
+++ Client/src/main/java/de/baleipzig/pdfextraction/client/utils/views/FXMLView.java
@@ -37,15 +37,16 @@ import static java.util.concurrent.CompletableFuture.supplyAsync;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * #L%
+ *
  * @author adam-bien.com
  */
 public abstract class FXMLView extends StackPane {
@@ -108,11 +109,10 @@ public abstract class FXMLView extends StackPane {
 
         if (factories.size() == 1) {
             return factories.get(0);
-        } else {
-            factories.forEach(System.err::println);
-            throw new IllegalStateException("More than one PresenterFactories discovered");
         }
 
+        factories.forEach(f -> LoggerFactory.getLogger(FXMLView.class).error("More than 1 PresenterFactory found {}", f));
+        throw new IllegalStateException("More than one PresenterFactories discovered");
     }
 
     void initializeFXMLLoader() {
