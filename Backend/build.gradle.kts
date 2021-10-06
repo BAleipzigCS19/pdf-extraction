d75643f50f3f5a0dbb72ddea3769c4424ddaa0d0
WillyTheBee <wi1999ku@gmail.com>
2021-10-06 17:16:34 +0200

    PAN-5 test

--- Backend/build.gradle.kts
+++ Backend/build.gradle.kts
@@ -2,11 +2,16 @@ version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
     // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+
+    // https://github.com/Dansoftowner/PDFViewerFX
+    implementation("com.github.Dansoftowner:PDFViewerFX:0.8")
 }
 
 tasks.test {
---
+++ Backend/src/main/java/de/baleipzig/pdfextraction/backend/Pdf_viewer.java
@@ -0,0 +1,7 @@
+package de.baleipzig.pdfextraction.backend;
+
+
+public class Pdf_viewer {
+
+
+}
