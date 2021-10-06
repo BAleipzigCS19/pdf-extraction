dc3c2d26bbc8172ad72030eea92f4a0de077c8c1
krankkk <59603675+krankkkk@users.noreply.github.com>
2021-10-07 01:05:38 +0200

    PAN-19 Progress so far

--- Client/build.gradle.kts
+++ Client/build.gradle.kts
@@ -31,11 +31,12 @@ dependencies {
     // https://mvnrepository.com/artifact/jakarta.annotation/jakarta.annotation-api
     implementation("jakarta.annotation:jakarta.annotation-api:2.1.0-B1")
 
-
     // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
-    runtimeOnly("com.fasterxml.jackson.core:jackson-databind:2.13.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
+
     compileOnly("org.slf4j:slf4j-api:2.0.0-alpha5")
     runtimeOnly("ch.qos.logback:logback-classic:1.3.0-alpha10")
+
 
     // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
@@ -43,7 +44,7 @@ dependencies {
     // https://mvnrepository.com/artifact/io.projectreactor/reactor-test
     testImplementation("io.projectreactor:reactor-test:3.4.10")
     // https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
-    implementation("com.squareup.okhttp3:okhttp:4.9.2")
+    testImplementation("com.squareup.okhttp3:okhttp:4.9.2")
     // https://mvnrepository.com/artifact/com.squareup.okhttp3/mockwebserver
     testImplementation("com.squareup.okhttp3:mockwebserver:4.9.2")
 }
