9df2abd40f77b552cffb40515ab7888f71b275f1
krankkk <59603675+krankkkk@users.noreply.github.com>
2021-10-10 17:44:25 +0200

    PAN-19

--- Client/src/main/java/de/baleipzig/pdfextraction/client/controller/ImportController.java
+++ Client/src/main/java/de/baleipzig/pdfextraction/client/controller/ImportController.java
@@ -1,5 +1,6 @@
 package de.baleipzig.pdfextraction.client.controller;
 
+import de.baleipzig.pdfextraction.client.PDFPreview;
 import de.baleipzig.pdfextraction.client.connector.TemplateConnector;
 import de.baleipzig.pdfextraction.client.utils.AlertUtils;
 import de.baleipzig.pdfextraction.client.utils.ControllerUtils;
@@ -12,13 +13,25 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.UncheckedIOException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.function.Supplier;
 
 public class ImportController implements Initializable {
+
+    protected static final PDFPreview renderer = new PDFPreview();
 
     @Inject
     private TemplateConnector connector;
@@ -28,6 +41,24 @@ public class ImportController implements Initializable {
 
     @FXML
     public ComboBox<String> templateComboBox;
+
+    @FXML
+    public Button createTemplateButton;
+
+    @FXML
+    public Label pageIndex;
+
+    @FXML
+    public Button buttonPageBack;
+
+    @FXML
+    public Button buttonPageForward;
+
+    @FXML
+    public ImageView imageView;
+
+    @FXML
+    public Button buttonChooseFile;
 
     @FXML
     private void continueButtonOnAction() {
@@ -41,6 +72,17 @@ public class ImportController implements Initializable {
                 .doOnError(err -> LoggerFactory.getLogger(ImportController.class).error("Exception while listening for response.", err))
                 .doOnError(err -> Platform.runLater(() -> AlertUtils.showAlert(Alert.AlertType.ERROR, "Fehler", "", "Es ist ein Fehler bei dem kommunizieren mit dem Server aufgetreten.")))
                 .subscribe(this::onRequestCompleted);
+
+        // image view resizeble machen
+        AnchorPane parent = (AnchorPane) imageView.getParent();
+        imageView.fitWidthProperty().bind(parent.widthProperty());
+        imageView.fitHeightProperty().bind(parent.heightProperty());
+
+        if (renderer.hasPreview()) {
+            updatePdfPreview(renderer::getCurrentPreview);
+        } else {
+            pageIndex.setText("Seitenanzahl");
+        }
     }
 
     private void onRequestCompleted(final String name) {
@@ -55,4 +97,56 @@ public class ImportController implements Initializable {
                 new CreateTemplate()
         );
     }
+
+    @FXML
+    public void onClickPageBack() {
+
+        if (renderer.hasPreviousPage()) {
+            updatePdfPreview(renderer::getPreviousPreview);
+        }
+    }
+
+    @FXML
+    public void onClickPageForward() {
+
+        if (renderer.hasNextPage()) {
+            updatePdfPreview(renderer::getNextPreview);
+        }
+    }
+
+    @FXML
+    public void onClickChooseFile() {
+        // Filechooser
+        final Stage current = (Stage) this.buttonChooseFile.getScene().getWindow();
+        final FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().setAll(List.of(new FileChooser.ExtensionFilter("PDF's", "*.pdf")));
+        final File selectedFile = fileChooser.showOpenDialog(current);
+        if (selectedFile == null) {
+            //User canceled the dialog
+            return;
+        }
+
+        final Path pdfPath = selectedFile.toPath();
+
+        renderer.setPdfPath(pdfPath);
+        // die aktuelle Seitenzahl soll resetet werden wenn eine neue Datei geladen wird
+        updatePdfPreview(renderer::getCurrentPreview);
+    }
+
+    private void updatePdfPreview(final Supplier<Image> image) {
+        try {
+            final Image toSet = image.get();
+
+            imageView.setImage(toSet);
+            pageIndex.setText("Seite: %d/%d".formatted(renderer.getCurrentPage() + 1, renderer.getNumberOfPages() + 1));
+        } catch (final UncheckedIOException | IllegalStateException e) {
+            LoggerFactory.getLogger(ImportController.class)
+                    .atError()
+                    .setCause(e)
+                    .log("Exception occurred while setting new Image.");
+
+            AlertUtils.showAlert(Alert.AlertType.ERROR, "Fehler", "Ein Fehler ist aufgetreten.", e.getLocalizedMessage());
+        }
+    }
+
 }
