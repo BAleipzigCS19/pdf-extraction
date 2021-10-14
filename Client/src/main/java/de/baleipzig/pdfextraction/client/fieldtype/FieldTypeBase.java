9756d4bf0dd5cbbd0843e9c6301167617af89279
krankkk <59603675+krankkkk@users.noreply.github.com>
2021-10-14 23:16:43 +0200

    PAN-19

--- Client/src/main/java/de/baleipzig/pdfextraction/client/fieldtype/FieldTypeBase.java
+++
@@ -1,8 +0,0 @@
-package de.baleipzig.pdfextraction.client.fieldtype;
-
-import de.baleipzig.pdfextraction.api.fields.ApiBase;
-
-public interface FieldTypeBase {
-
-    ApiBase analyze(String text);
-}
