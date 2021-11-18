317bc70871d22f74589b7cacc8691d084ea6ac36
MartinSpiegel <martin_spiegel@gmx.de>
2021-11-18 13:10:04 +0100

    PAN-53 - Issues behoben - FXTests begonnen

--- Client/src/test/java/de/baleipzig/pdfextraction/client/controller/ActionControllerTest.java
+++ Client/src/test/java/de/baleipzig/pdfextraction/client/controller/ActionControllerTest.java
@@ -1,8 +1,11 @@
 package de.baleipzig.pdfextraction.client.controller;
 
+import com.jfoenix.controls.JFXMasonryPane;
 import de.baleipzig.pdfextraction.client.utils.SceneHandler;
 import de.baleipzig.pdfextraction.client.utils.injector.Injector;
 import de.baleipzig.pdfextraction.client.view.Actions;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
@@ -20,6 +23,8 @@ import static org.testfx.api.FxAssert.verifyThat;
 @EnabledOnOs({OS.WINDOWS, OS.MAC})
 class ActionControllerTest extends ApplicationTest {
 
+    private Stage stage;
+
     @BeforeAll
     static void beforeAll() {
         Locale.setDefault(Locale.GERMAN);
@@ -27,6 +32,7 @@ class ActionControllerTest extends ApplicationTest {
 
     @Override
     public void start(Stage stage) {
+        this.stage = stage;
         Injector.createInstance(SceneHandler.class).switchScene(stage, new Actions());
     }
 
@@ -35,7 +41,6 @@ class ActionControllerTest extends ApplicationTest {
 
         verifyThat("#runActionButton", NodeMatchers.isVisible());
         verifyThat("#backToImportButton", NodeMatchers.isVisible());
-        verifyThat("#createTerminationCheckBox", NodeMatchers.isVisible());
     }
 
     @Test
@@ -43,6 +48,17 @@ class ActionControllerTest extends ApplicationTest {
 
         clickOn("#backToImportButton");
         verifyThat("#continueButton", NodeMatchers.isVisible());
+    }
+
+    @Test
+    void selectMoreThanOneAction() {
+
+        ObservableList<Node> items = ((JFXMasonryPane)(stage.getScene().lookup("#contentPane"))).getChildren();
+        clickOn(items.get(0).lookup("#selectActionCircle"));
+        clickOn(items.get(1).lookup("#selectActionCircle"));
+        clickOn("#runActionButton");
+
+        verifyThat("Es darf nur eine Aktion ausgewählt werden.",NodeMatchers.isVisible());
     }
 
     @Test
